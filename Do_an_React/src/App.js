0f7273301de8a86cc1c36f4358a3f9215a0fc3c9
Xuan Nguyen <nguyenthihaxuan040900hiie@gmail.com>
2021-03-29 21:23:40 +0700

    heroku cli

---
+++ Do_an_React/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Đồ án cuối khóa</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
